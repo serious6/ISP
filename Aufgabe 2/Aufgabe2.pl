40773297b7b8e2c00c65f8ebeaa3c798dc3bb0e5
serious6 <andre.behrens@haw-hamburg.de>
2014-11-10 16:31:04 +0100

    ...

--- Aufgabe 2/Aufgabe2.pl
+++ Aufgabe 2/Aufgabe2.pl
@@ -1,3 +1,18 @@
 % Laden der externen Dateien
 :- ensure_loaded([readsentence]).
 :- ensure_loaded([stammbaum]).
+
+startup :-
+        writeln('Bitte stellen Sie eine Anfrage...'),
+        read_sentence(In),
+        strip(Result, In),
+        writeln(Result)
+        .
+        
+post_question --> open_question.
+post_question --> closed_question.
+        
+        
+strip(Q,In) :- append(Q,['.'],In).
+strip(Q,In) :- append(Q,['?'],In).
+strip(Q,In) :- append(Q,['!'],In).
