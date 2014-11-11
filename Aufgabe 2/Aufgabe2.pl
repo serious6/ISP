44ce811f17a9867d26a2e2ba575ee00101b2d008
vince <chuckySTAR@gmail.com>
2014-11-11 21:01:36 +0100

    rc1

--- Aufgabe 2/Aufgabe2.pl
+++ Aufgabe 2/Aufgabe2.pl
@@ -1,18 +1,42 @@
-% Laden der externen Dateien
 :- ensure_loaded([readsentence]).
 :- ensure_loaded([stammbaum]).
 
-startup :-
-        writeln('Bitte stellen Sie eine Anfrage...'),
-        read_sentence(In),
-        strip(Result, In),
-        writeln(Result)
-        .
-        
-post_question --> open_question.
-post_question --> closed_question.
-        
-        
-strip(Q,In) :- append(Q,['.'],In).
-strip(Q,In) :- append(Q,['?'],In).
-strip(Q,In) :- append(Q,['!'],In).
+start :-
+	writeln('Was willst du denn?'),
+	read_sentence(Satz),
+	frage(Satz, []).
+
+% Ergänzungsfrage
+% Wer ist der Bruder von Chantal?
+frage -->
+	interrogativpronomen,
+	verb,
+	anpe(Funk, Name),
+	{
+		Funktion =.. [Funk, Wer, Name],
+		call(Funktion),
+		write(Wer)
+	}.
+	
+% Artikel, Nomen, Präposition, Eigenname
+anpe(Funk, Name) -->
+	artikel,
+	nomen(Funk),
+	praeposition,
+	eigenname(Name).
+
+interrogativpronomen --> [X], {lex(X, _, interrogativpronomen)}.
+praeposition --> [X], {lex(X, _, praeposition)}.
+eigenname(Name) --> [Name], {lex(Name, _, eigenname)}.
+artikel --> [X], {lex(X, _, artikel)}.
+nomen(Funk) --> [X], {lex(X, Funk, nomen)}.
+verb --> [X], {lex(X, _, verb)}.
+
+lex(wer, _, interrogativpronomen).
+lex(ist, _, verb).
+lex(der, _, artikel).
+lex(bruder, brother, nomen).
+lex(von, _, praeposition).
+
+lex(Name, _, eigenname) :- male(Name).
+lex(Name, _, eigenname) :- female(Name).
