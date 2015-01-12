492b83799dad54d0b909756652c2af80df5ff8b9
serious6 <andre.behrens@haw-hamburg.de>
2015-01-12 15:44:54 +0100

    ...

--- Aufgabe 4/src/main/java/de/haw/resolver/Propagator.java
+++ Aufgabe 4/src/main/java/de/haw/resolver/Propagator.java
@@ -1,12 +1,37 @@
 package de.haw.resolver;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
-public class Propagator {
+public class Propagator<T> implements Iterable<T> {
 
 	protected String name;
 
-	protected List<Object> values = new ArrayList<Object>();
+	protected List<T> values = new ArrayList<T>();
 
+	public Propagator(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void addValue(T value) {
+		values.add(value);
+	}
+
+	public T getValue(int index) {
+		return values.get(index);
+	}
+
+	public void empty() {
+		values = new ArrayList<T>();
+	}
+
+	@Override
+	public Iterator<T> iterator() {
+		return values.iterator();
+	}
 }
