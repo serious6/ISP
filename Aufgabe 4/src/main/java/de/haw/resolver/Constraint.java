925c46560b3b760ce1f35e4d67c42591eed18cb6
serious6 <andre.behrens@haw-hamburg.de>
2015-01-12 15:46:02 +0100

    ...

--- Aufgabe 4/src/main/java/de/haw/resolver/Constraint.java
+++ Aufgabe 4/src/main/java/de/haw/resolver/Constraint.java
@@ -2,24 +2,24 @@ package de.haw.resolver;
 
 public abstract class Constraint {
 
-	protected Propagator left;
-	protected Propagator right;
+	protected Propagator<?> left;
+	protected Propagator<?> right;
 
-	protected Propagator result;
+	protected Propagator<?> result;
 
-	public Propagator getLeft() {
+	public Propagator<?> getLeft() {
 		return left;
 	}
 
-	public void setLeft(Propagator left) {
+	public void setLeft(Propagator<?> left) {
 		this.left = left;
 	}
 
-	public Propagator getRight() {
+	public Propagator<?> getRight() {
 		return right;
 	}
 
-	public void setRight(Propagator right) {
+	public void setRight(Propagator<?> right) {
 		this.right = right;
 	}
 
