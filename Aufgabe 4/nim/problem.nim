8b36f00371b86a3c698428d78323c3ca7d43a6fe
vince <chuckySTAR@gmail.com>
2015-01-14 23:39:34 +0100

    kotmentare für businessmensch

--- Aufgabe 4/nim/problem.nim
+++ Aufgabe 4/nim/problem.nim
@@ -21,7 +21,6 @@ proc addNeighbours(problem: var Problem, constraint: Constraint, queue: var seq[
 proc cmp(constraint: Constraint, a, b: int): bool
 proc arcReduce(problem: var Problem, constraint: Constraint): bool
 proc addAllBinaryConstraints(problem: var Problem, queue: var seq[Constraint])
-proc printQueue(queue: var seq[Constraint])
 proc solve2(problem: var Problem, queue: var seq[Constraint]): bool
 proc ac3la(problem: var Problem, queue: var seq[Constraint]): bool
 proc saveDomains(problem: var Problem): seq[DomainBackup]
@@ -51,8 +50,6 @@ proc addConstraint*(problem: var Problem, constraint: Constraint) =
     c2.right = constraint.left
     c2.value = constraint.value
     c2.op = constraint.op
-    #if constraint.custom != nil:
-    #  c2.custom = proc (a, b: int): bool = constraint.custom(b, a)
     problem.constraints.add(c2)
 
 proc addConstraint*(problem: var Problem, custom: proc (a, b: int): bool, a, b: string) =
@@ -98,7 +95,6 @@ proc solve2(problem: var Problem, queue: var seq[Constraint]): bool =
         var domain: seq[int] = problem.variables[k].domain
         for value in domain:
           var backup: seq[DomainBackup] = problem.saveDomains()
-          #echo("test " & k & " = " & $(value))
           problem.variables.mget(k).domain = @[value]
           var queue2: seq[Constraint] = @[]
           problem.addInvolvedConstraints(k, queue2)
@@ -112,18 +108,20 @@ proc solve2(problem: var Problem, queue: var seq[Constraint]): bool =
   return true
     
 proc saveDomains(problem: var Problem): seq[DomainBackup] =
+  ## speichert domänen aller variabeln und gibt sie als liste zurück
   result = @[]
   for k, v in pairs(problem.variables):
     var backup: DomainBackup = (k, v.domain)
     result.add(backup)
 
 proc restoreDomains(problem: var Problem, domains: seq[DomainBackup]) =
+  ## überschreibt die domänen aller variablen mit der gegebenen liste von DomainBackup tuplen
   for v in domains:
     problem.variables.mget(v.name).domain = v.domain
 
 proc ac3la(problem: var Problem, queue: var seq[Constraint]): bool =
+  ## AC-3 LookAhead
   result = true
-  #printQueue(queue)
   
   while queue.len() > 0 and result:
     var c: Constraint = queue[0]
@@ -135,15 +133,14 @@ proc ac3la(problem: var Problem, queue: var seq[Constraint]): bool =
       #echo("REVISE(" & c.left & ", " & c.right & ") bewirkt nichts")
     #printQueue(queue)
 
-proc arcReduce(problem: var Problem, constraint: Constraint): bool = # aka REVISE
+proc arcReduce(problem: var Problem, constraint: Constraint): bool =
+  ## REVISE
   var len = problem.variables[constraint.left].domain.len()
   var i = len - 1
   while i >= 0:
     var found = false
     var v1: int = problem.variables[constraint.left].domain[i]
-    #echo(constraint.left & " domain " & $(problem.variables[constraint.left].domain))
     for v2 in problem.variables[constraint.right].domain:
-      #echo(constraint.left & " = " & $(v1) & " cmp " & constraint.right & " = " & $(v2) & " === " & $(constraint.cmp(v1, v2)))
       if constraint.cmp(v1, v2):
         found = true
         break
@@ -155,26 +152,21 @@ proc arcReduce(problem: var Problem, constraint: Constraint): bool = # aka REVIS
     #echo("REVISE(" & constraint.left & ", " & constraint.right & ") bewirkt " & constraint.left & " ∈ " & $(problem.variables[constraint.left].domain))
 
 proc addAllBinaryConstraints(problem: var Problem, queue: var seq[Constraint]) =
+  ## fügt alle constraints in die übergebene queue
   for constraint in problem.constraints:
     queue.add(constraint)
 
 proc addNeighbours(problem: var Problem, constraint: Constraint, queue: var seq[Constraint]) =
+  ## fügt alle constraints in die übergebene queue hinzu, die rechts die gleiche variable besitzen, wie das übergebene constraint auf der linken seite
   for c in problem.constraints:
-    if constraint.left == c.right and c.left != constraint.right: # before: rückwärtskante WAR NICHT in problem.constraints drin
-      #echo("(" & c.left & ", " & c.right & ") gefunden zum adden")
+    if constraint.left == c.right and c.left != constraint.right:
       queue.insert(c, 0)
 
 proc addInvolvedConstraints(problem: var Problem, variable: string, queue: var seq[Constraint]) =
+  ## fügt alle constraints in die übergebene queue hinzu, die an dieser variable beteiligt sind
   for c in problem.constraints:
     if c.right == variable and c.left != variable:
       queue.insert(c, 0)
-
-proc printQueue(queue: var seq[Constraint]) =
-  var s = "{"
-  for c in queue:
-    s = s & "(" & c.left & ", " & c.right & "), "
-  s = s & "}"
-  echo(s)
 
 proc cmp(constraint: Constraint, a, b: int): bool =
   case constraint.op
